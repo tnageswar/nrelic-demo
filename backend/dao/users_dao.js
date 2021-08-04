7bb1723d756bbc996d2b11580a5eb74a78c70136
Nageswararao T <tnageswar22@gmail.com>
2021-08-04 22:40:52 +0530

    Backend : Added User Id

--- backend/dao/users_dao.js
+++ backend/dao/users_dao.js
@@ -1,19 +1,29 @@
 const data = require('../database/data.json');
 const logger = require('../utils/logger');
+const headers = [
+    { field: 'uid', headerName: 'UID' },
+    {
+        field: 'userName',
+        headerName: 'User Name',
+    },
+    {
+        field: 'company_name',
+        headerName: 'Company',
+    },
+];
 function getAllUsers(inOffset, inLimit) {
     const start = inOffset ?? 0;
     const end = +start + (+inLimit ?? 10);
     logger.debug(`DAO Pagination[start:${start}, end:${end}]`);
     return {
-        users: data
-            .map((user) => {
-                return {
-                    first_name: user['first_name'],
-                    last_name: user['last_name'],
-                    company_name: user['company_name'],
-                };
-            })
-            .slice(start, end),
+        headers,
+        users: data.slice(start, end).map((user) => {
+            return {
+                uid: user['uid'],
+                userName: `${user['first_name']}, ${user['last_name']}`,
+                companyName: user['company_name'],
+            };
+        }),
     };
 }
 
--- backend/database/temputil.js
+++ backend/database/temputil.js
@@ -1,39 +1,40 @@
-const data = require('./withcompany_1627993565486.json');
+const data = require('./original_csv_to_json.json');
 
 console.log(`The data length is ${data.length}`);
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-const comset = new Set();
-data.forEach((ele) => {
-    comset.add(ele['company_name']);
-});
-
-comset.forEach((element) => {
-    console.log(element);
-});
-
-// Just randomly selecting companies
-// const comp_arr = [];
-// for (let i = 0; i < 8; i++) {
-//     let indx = getRandomInt(499);
-//     console.log(`${data[indx]['company_name']}`);
-//     comp_arr.push(data[indx]['company_name']);
-// }
-
+// const comset = new Set();
 // data.forEach((ele) => {
-//     ele.company_name = comp_arr[getRandomInt(7)];
-//     console.log(`comp: ${ele.company_name}`);
+//     comset.add(ele['company_name']);
 // });
 
-// const fs = require('fs');
+// comset.forEach((element) => {
+//     console.log(element);
+// });
 
-// const content = JSON.stringify(data);
+//Just randomly selecting companies
+const comp_arr = [];
+for (let i = 0; i < 8; i++) {
+    let indx = getRandomInt(499);
+    console.log(`${data[indx]['company_name']}`);
+    comp_arr.push(data[indx]['company_name']);
+}
 
-// try {
-//     const data = fs.writeFileSync(`./withcompany_${Date.now()}.json`, content);
-//     //file written successfully
-// } catch (err) {
-//     console.error(err);
-// }
+data.forEach((ele, idx) => {
+    ele.company_name = comp_arr[getRandomInt(7)];
+    ele.uid = 1000 + idx;
+    console.log(`comp: ${ele.company_name}`);
+});
+
+const fs = require('fs');
+
+const content = JSON.stringify(data);
+
+try {
+    const data = fs.writeFileSync(`./withcompany_${Date.now()}.json`, content);
+    //file written successfully
+} catch (err) {
+    console.error(err);
+}
